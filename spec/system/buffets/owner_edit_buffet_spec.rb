361889efe01913554131480cc15f6d9bf15e62db
Lucas <obxlucas@gmail.com>
2024-04-20 20:24:09 -0300

    feat: meios de pagamento na edição de buffet

--- spec/system/buffets/owner_edit_buffet_spec.rb
+++ spec/system/buffets/owner_edit_buffet_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe 'Proprietário edita um buffet' do
   it 'a partir da página de detalhes' do
     owner = Owner.create!(name: 'Jorge', email: 'jorge@email.com', password: '12345678')
+    credit = PaymentMethod.create!(name: 'Cartão de Crédito')
+    cash = PaymentMethod.create!(name: 'Dinheiro')
     Buffet.create!(
       brand_name: 'Casamentos Buffet',
       corporate_name: 'Casamentos Buffet LTDA',
@@ -15,7 +17,8 @@ describe 'Proprietário edita um buffet' do
       state: 'SP',
       postal_code: '14980-970',
       description: 'Buffet especializado em casamentos',
-      owner: owner)
+      owner: owner,
+      payment_methods: [credit, cash])
     
     login_as(owner)
     visit root_path
@@ -34,10 +37,14 @@ describe 'Proprietário edita um buffet' do
     expect(page).to have_field 'Estado', with: 'SP'
     expect(page).to have_field 'CEP', with: '14980-970'
     expect(page).to have_field 'Descrição', with: 'Buffet especializado em casamentos'
+    expect(page).to have_content 'Cartão de Crédito'
+    expect(page).to have_content 'Dinheiro'
   end
 
   it 'com sucesso' do
     owner = Owner.create!(name: 'Jorge', email: 'jorge@email.com', password: '12345678')
+    credit = PaymentMethod.create!(name: 'Cartão de Crédito')
+    cash = PaymentMethod.create!(name: 'Dinheiro')
     Buffet.create!(
       brand_name: 'Casamentos Buffet',
       corporate_name: 'Casamentos Buffet LTDA',
@@ -50,7 +57,8 @@ describe 'Proprietário edita um buffet' do
       state: 'SP',
       postal_code: '14980-970',
       description: 'Buffet especializado em casamentos',
-      owner: owner)
+      owner: owner,
+      payment_methods: [credit, cash])
     
     login_as(owner)
     visit root_path
@@ -58,16 +66,21 @@ describe 'Proprietário edita um buffet' do
     click_on 'Editar'
     fill_in 'Nome Fantasia', with: 'Casa Buffet'
     fill_in 'Endereço', with: 'Av Pedra Grande, 55'
+    uncheck 'Dinheiro'
     click_on 'Enviar'
 
     expect(page).to have_content 'Buffet atualizado com sucesso.'
     expect(page).to have_content 'Casa Buffet'
     expect(page).to have_content 'Endereço: Av Pedra Grande, 55 - Jardim do Sol, Sales - SP - 14980-970'
+    expect(page).to have_content 'Cartão de Crédito'
+    expect(page).not_to have_content 'Dinheiro'
   end
 
   it 'e não consegue editar o buffet de outro usuário' do
     owner = Owner.create!(name: 'Jorge', email: 'jorge@email.com', password: '12345678')
     second_owner = Owner.create!(name: 'Julia', email: 'julia@email.com', password: '45688520')
+    credit = PaymentMethod.create!(name: 'Cartão de Crédito')
+    cash = PaymentMethod.create!(name: 'Dinheiro')
     buffet = Buffet.create!(
       brand_name: 'Casamentos Buffet',
       corporate_name: 'Casamentos Buffet LTDA',
@@ -80,8 +93,9 @@ describe 'Proprietário edita um buffet' do
       state: 'SP',
       postal_code: '14980-970',
       description: 'Buffet especializado em casamentos',
-      owner: owner)
-    Buffet.create!(
+      owner: owner,
+      payment_methods: [credit, cash])
+    second_buffet = Buffet.create!(
       brand_name: 'Edecy Buffet',
       corporate_name: 'Edecy Buffet LTDA',
       registration_code: '55996244000122',
@@ -93,7 +107,8 @@ describe 'Proprietário edita um buffet' do
       state: 'MG',
       postal_code: '55280-001',
       description: 'Buffet para festa infantil',
-      owner: second_owner)
+      owner: second_owner,
+      payment_methods: [credit, cash])
 
       login_as(second_owner)
       visit edit_buffet_path(buffet)
